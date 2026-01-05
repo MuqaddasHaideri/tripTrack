45f59621b692b9392aa3e0819cff8fe022a7d718
Muqaddas Haideri <khanmuqaddas667@gmail.com>
2026-01-05 16:21:45 +0500

    changed icon

--- tripTrack/app/(tabs)/_layout.tsx
+++ tripTrack/app/(tabs)/_layout.tsx
@@ -5,7 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
+import Fontisto from '@expo/vector-icons/Fontisto';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -27,7 +27,7 @@ export default function TabLayout() {
         name="setting"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gearshape.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Fontisto name="player-settings" size={24} color={color} />,
         }}
       />
     </Tabs>
