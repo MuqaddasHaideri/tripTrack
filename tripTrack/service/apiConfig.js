b2f3ef37173d457bc008450a332245cf9587ec9f
Muqaddas Haideri <khanmuqaddas667@gmail.com>
2026-05-31 17:53:31 +0500

    apiconfig baseurl

--- tripTrack/service/apiConfig.js
+++ tripTrack/service/apiConfig.js
@@ -1,7 +1,7 @@
 
 
- export const API_BASE = "http://192.168.0.104:3001/api";
-// export const API_BASE = "https://fyp-atio.onrender.com/api"; 
+//  export const API_BASE = "http://192.168.0.104:3001/api";
+export const API_BASE = "https://fyp-atio.onrender.com/api"; 
 // export const API_BASE = "https://fyp-atio.onrender.com/api"; 
 export const endpoints = {
   login: "/auth/login",
